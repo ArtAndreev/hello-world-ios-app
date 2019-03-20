070b62a89535b65638530fe8f0511727cb9b983d
Artyom Andreev <aandreev06.1998@gmail.com>
2019-03-20 19:15:02 +0300

    Initial Commit

---
+++ ios-task1Tests/ios_task1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_task1Tests.swift
+//  ios-task1Tests
+//
+//  Created by artyom.andreev on 20/03/2019.
+//  Copyright © 2019 artyom.andreev. All rights reserved.
+//
+
+import XCTest
+@testable import ios_task1
+
+class ios_task1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
